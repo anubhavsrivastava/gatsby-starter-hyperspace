40e777cea8527211852ab04dd16f85d34788e679
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-11 17:02:41 +0530

    added basic gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter Hyperspace',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Hyperspace',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'standalone',
+        icon: 'src/assets/img/website-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+  ],
+}
