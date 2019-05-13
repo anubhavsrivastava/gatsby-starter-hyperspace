f54f71ce19e3d2b241affab9d11a727b1c249d98
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-13 20:38:06 +0530

    fixed pathPrefix path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/gatsby-starter-hyperspace`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/gatsby-starter-hyperspace/`, // This path is subpath of your hosting https://domain/portfolio
   siteMetadata: {
     title: 'Gatsby Starter Hyperspace',
   },
