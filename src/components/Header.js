d882ce8d45a94b8debd6ae50d7a29eef92752c44
casterminator <44626745+casterminator@users.noreply.github.com>
2019-06-18 14:00:47 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import '../assets/sass/main.scss';
 const Header = () => (
   <header id="header">
     <Link className="title" to="/">
-      Hyperspace
+      Dinero
     </Link>
     <nav>
       <ul>
