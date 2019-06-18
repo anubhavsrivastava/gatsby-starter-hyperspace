fe698fd1dffa7448519e51104ceef76b6051ccec
casterminator <44626745+casterminator@users.noreply.github.com>
2019-06-18 13:25:03 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
   <footer id="footer" className="wrapper style1-alt">
     <div className="inner">
       <ul className="menu">
-        <li>&copy; Untitled. All rights reserved.</li>
+        <li>Dinero Coin Project. All rights reserved.</li>
         <li>
           Design: <a href="http://html5up.net">HTML5 UP</a>
         </li>
