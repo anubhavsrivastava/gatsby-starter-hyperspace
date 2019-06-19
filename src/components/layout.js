73946d40022d2ed8dc04bb456ddd1391b8a97ac5
casterminator <44626745+casterminator@users.noreply.github.com>
2019-06-19 12:22:24 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,7 +20,7 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Hyperspace' },
+            { name: 'description', content: 'Dinero Coin' },
             { name: 'keywords', content: 'site, web' },
           ]}
         >
