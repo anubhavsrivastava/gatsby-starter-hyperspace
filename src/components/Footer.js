36aa0f164eb27fb4b06e1b9f7521ac097d991fe1
casterminator <44626745+casterminator@users.noreply.github.com>
2019-06-19 13:39:42 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
   <footer id="footer" className="wrapper style1-alt">
     <div className="inner">
       <ul className="menu">
-        <li>Dinero Coin Project. All rights reserved 2018.</li>
+        <li>Dinero Coin Project. All rights reserved 2019.</li>
         <li>
           Design: <a href="https://github.com/anubhavsrivastava/gatsby-starter-hyperspace">Anubhav Srivastava</a>
         </li>
