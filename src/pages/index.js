45e863f0b253f083127a5b897b57385d7aacbb18
casterminator <44626745+casterminator@users.noreply.github.com>
2019-06-19 10:37:17 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -169,31 +169,6 @@ const IndexPage = () => (
           </p>
           <div className="split style1">
             <section>
-              <form method="post" action="#">
-                <div className="fields">
-                  <div className="field half">
-                    <label for="name">Name</label>
-                    <input type="text" name="name" id="name" />
-                  </div>
-                  <div className="field half">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" />
-                  </div>
-                  <div className="field">
-                    <label for="message">Message</label>
-                    <textarea name="message" id="message" rows="5" />
-                  </div>
-                </div>
-                <ul className="actions">
-                  <li>
-                    <a href="/#" className="button submit">
-                      Send Message
-                    </a>
-                  </li>
-                </ul>
-              </form>
-            </section>
-            <section>
               <ul className="contact">
                 <li>
                   <h3>Email</h3>
@@ -240,6 +215,31 @@ const IndexPage = () => (
                   </ul>
                 </li>
               </ul>
+            </section>
+            <section>
+              <form method="post" action="#">
+                <div className="fields">
+                  <div className="field half">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" />
+                  </div>
+                  <div className="field half">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" />
+                  </div>
+                  <div className="field">
+                    <label for="message">Message</label>
+                    <textarea name="message" id="message" rows="5" />
+                  </div>
+                </div>
+                <ul className="actions">
+                  <li>
+                    <a href="/#" className="button submit">
+                      Send Message
+                    </a>
+                  </li>
+                </ul>
+              </form>
             </section>
           </div>
         </div>
